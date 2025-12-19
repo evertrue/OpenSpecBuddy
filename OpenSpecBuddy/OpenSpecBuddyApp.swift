b6b3973d4a3fa48689fd9724eaa06c19c424e332
PJ Gray <pj4533@gmail.com>
2025-12-19 07:57:25 -0500

    Initial Commit

---
+++ OpenSpecBuddy/OpenSpecBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSpecBuddyApp.swift
+//  OpenSpecBuddy
+//
+//  Created by PJ Gray on 12/19/25.
+//
+
+import SwiftUI
+
+@main
+struct OpenSpecBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OpenSpecBuddyTests/OpenSpecBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSpecBuddyTests.swift
+//  OpenSpecBuddyTests
+//
+//  Created by PJ Gray on 12/19/25.
+//
+
+import Testing
+@testable import OpenSpecBuddy
+
+struct OpenSpecBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
